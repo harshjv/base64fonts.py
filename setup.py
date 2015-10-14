4d374c0964bbea7a4ce333e4f0c841bc1cbcc6d2
Harsh Vakharia <harshjv@gmail.com>
2015-10-14 20:28:29 +0530

    Rename convert to base64fonts

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 
 setup(
     name='base64fonts',
-    version='1.0',
+    version='0.1',
     author='Harsh Vakharia',
-    py_modules=['convert'],
+    py_modules=['base64fonts'],
     install_requires=['colorama'],
     license='MIT',
     keywords='fonts less sass converter',
     entry_points={
-        'console_scripts': ['base64fonts = convert:main']
+        'console_scripts': ['base64fonts = base64fonts:main']
     })
